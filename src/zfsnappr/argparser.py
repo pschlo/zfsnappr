231ef7eb7e30d82c7ca10a47a7e28730485de9fb
pschlo <70105856+pschlo@users.noreply.github.com>
2026-01-22 15:57:37 +0000

    updated argparsing

--- src/zfsnappr/argparser.py
+++ src/zfsnappr/argparser.py
@@ -13,20 +13,51 @@ from . import (
 
 
 def get_args() -> argparse.Namespace:
-  # create top-level parser
-  parser = argparse.ArgumentParser('zfsnappr')
-  subparsers = parser.add_subparsers(dest="subcommand", required=True)
-  parser.add_argument('-d', '--dataset', type=str, metavar="DATASET")
-  parser.add_argument('-r', '--recursive', action='store_true')
-  parser.add_argument('-n', '--dry-run', action='store_true')
+    # Parent parser for global/common options
+    common = argparse.ArgumentParser(add_help=False, argument_default=argparse.SUPPRESS)
+    common.add_argument('-d', '--dataset', type=str, metavar="DATASET", help="asdf")
+    common.add_argument('-r', '--recursive', action='store_true', help="foooo")
+    common.add_argument('-n', '--dry-run', action='store_true')
+    DEFAULTS = dict(
+        dataset=None,
+        recursive=False,
+        dry_run=False
+    )
 
-  # create subcommand parsers
-  _list.argparser.setup(subparsers.add_parser('list'))
-  _create.argparser.setup(subparsers.add_parser('create'))
-  _prune.argparser.setup(subparsers.add_parser('prune'))
-  _push.argparser.setup(subparsers.add_parser('push'))
-  _pull.argparser.setup(subparsers.add_parser('pull'))
-  _tag.argparser.setup(subparsers.add_parser('tag'))
-  _version.argparser.setup(subparsers.add_parser('version'))
+    # create top-level parser
+    parser = argparse.ArgumentParser('zfsnappr', parents=[common], formatter_class=CompactHelpFormatter)
+    subparsers = parser.add_subparsers(dest="subcommand", required=True)
 
-  return parser.parse_args()
+    # create subcommand parsers
+    _list.argparser.setup(
+        subparsers.add_parser('list', parents=[common])
+    )
+    _create.argparser.setup(
+        subparsers.add_parser('create', parents=[common])
+    )
+    _prune.argparser.setup(
+        subparsers.add_parser('prune', parents=[common])
+    )
+    _push.argparser.setup(
+        subparsers.add_parser('push', parents=[common])
+    )
+    _pull.argparser.setup(
+        subparsers.add_parser('pull', parents=[common])
+    )
+    _tag.argparser.setup(
+        subparsers.add_parser('tag', parents=[common])
+    )
+    _version.argparser.setup(
+        subparsers.add_parser('version')
+    )
+
+    # Merge global defaults in
+    args = dict(parser.parse_args()._get_kwargs())
+    args = DEFAULTS | args
+
+    return argparse.Namespace(**args)
+
+
+class CompactHelpFormatter(argparse.HelpFormatter):
+    def __init__(self, prog):
+        super().__init__(prog, max_help_position=40, width=120)
