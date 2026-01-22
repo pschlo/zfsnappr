1182e9c7216d926d3dd3d63812abdfd3b93102f8
pschlo <70105856+pschlo@users.noreply.github.com>
2026-01-22 17:07:13 +0000

    update

--- src/zfsnappr/entrypoint.py
+++ src/zfsnappr/entrypoint.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from typing import cast
 import logging
 
-from .common.setup_logging import setup_logging
+from .setup_logging import setup_logging
 setup_logging()
 from .args import get_args
 from .commands import (
