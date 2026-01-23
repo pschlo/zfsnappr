9586b42deacb76d641add3f1ea113f104cbdf18c
pschlo <70105856+pschlo@users.noreply.github.com>
2026-01-23 11:13:19 +0000

    added parse_dataset method

--- src/zfsnappr/common/utils.py
+++ src/zfsnappr/common/utils.py
@@ -9,3 +9,39 @@ def group_snaps_by[T: Hashable](snapshots: Collection[Snapshot], get_group: Call
   for snap in snapshots:
     groups[get_group(snap)].append(snap)
   return groups
+
+
+class DatasetParseError(Exception):
+  def __init__(self) -> None:
+    super().__init__("Invalid dataset name")
+
+
+def parse_dataset(value: str):
+  user: str | None
+  host: str | None
+  dataset: str
+
+  _parts = value.split(':')
+  if not all(_parts):
+    raise DatasetParseError()
+  if len(_parts) == 1:
+    _netloc, dataset = None, _parts[0]
+  elif len(_parts) == 2:
+    _netloc, dataset = _parts
+  else:
+    raise DatasetParseError()
+
+  if _netloc is not None:
+    _parts = _netloc.split('@')
+    if not all(_parts):
+      raise DatasetParseError()
+    if len(_parts) == 1:
+      user, host = None, _parts[0]
+    elif len(_parts) == 2:
+      user, host = _parts
+    else:
+      raise DatasetParseError()
+  else:
+    user, host = None, None
+
+  return user, host, dataset
