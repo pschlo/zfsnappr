eb45f833f15c23ac440fc0ac2338625426e191d9
pschlo <70105856+pschlo@users.noreply.github.com>
2026-01-30 20:39:57 +0000

    update

--- src/zfsnappr/common/utils.py
+++ src/zfsnappr/common/utils.py
@@ -14,8 +14,8 @@ def group_snaps_by[T: Hashable](snapshots: Collection[Snapshot], get_group: Call
 
 
 class DatasetParseError(Exception):
-  def __init__(self, name: str) -> None:
-    super().__init__(f"Invalid dataset name '{name}'")
+  def __init__(self, spec: str) -> None:
+    super().__init__(f"Invalid dataset spec '{spec}'")
 
 
 ALNUM = set(string.ascii_letters + string.digits + '_-')
@@ -32,7 +32,7 @@ class DatasetConfig:
   dataset: str | None
 
 
-def parse_dataset(value: str):
+def parse_dataset_spec(spec: str):
   user: str | None
   host: str | None
   port: int | None
@@ -44,7 +44,7 @@ def parse_dataset(value: str):
   # value_resolved = user@host:port/dataset
 
   # split dataset path from domain/netloc
-  _parts = value.split('/', maxsplit=1)
+  _parts = spec.split('/', maxsplit=1)
   if len(_parts) == 1:
     _netloc, dataset = _parts[0], None
   elif len(_parts) == 2:
@@ -55,26 +55,26 @@ def parse_dataset(value: str):
   if _netloc is not None:
     _parts = _netloc.split('@')
     if not all(_parts):
-      raise DatasetParseError()
+      raise DatasetParseError(spec)
     if len(_parts) == 1:
       user, _hostport = None, _parts[0]
     elif len(_parts) == 2:
       user, _hostport = _parts
     else:
-      raise DatasetParseError()
+      raise DatasetParseError(spec)
   else:
     user, _hostport = None, None
 
   if _hostport is not None:
     _parts = _hostport.split(':')
     if not all(_parts):
-      raise DatasetParseError()
+      raise DatasetParseError(spec)
     if len(_parts) == 1:
       host, port = _parts[0], None
     elif len(_parts) == 2:
       host, port = _parts[0], int(_parts[1])
     else:
-      raise DatasetParseError()
+      raise DatasetParseError(spec)
   else:
     host, port = None, None
 
@@ -84,7 +84,7 @@ def parse_dataset(value: str):
     not host or is_alnum(host),
     not dataset or all(map(is_alnum, dataset.split('/')[1:]))
   ]):
-    raise DatasetParseError()
+    raise DatasetParseError(spec)
 
   return DatasetConfig(
     user=user,
@@ -98,7 +98,7 @@ def get_zfs_cli(value: str | None) -> tuple[ZfsCli, str | None]:
   if value is None:
     return LocalZfsCli(), None
 
-  config = parse_dataset(value)
+  config = parse_dataset_spec(value)
   if config.host:
     cli = RemoteZfsCli(
       host=config.host,
